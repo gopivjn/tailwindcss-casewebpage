20123eb336906779902cbb0d71234ecec7fe7cb0
gopivjn <gopivjn@gmail.com>
2020-06-28 23:20:16 +0530

    Layout done - styling and responsiveness pending

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {},
+  },
+  fontFamily: {
+    display: ["Roboto", "sans-serif"],
+    body: ["Roboto", "sans-serif"],
+  },
+  variants: {},
+  plugins: [],
+};
